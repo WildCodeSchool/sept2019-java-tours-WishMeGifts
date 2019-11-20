0228218db029f365304d137624fb1fe685064ea6
timlhote <timlhote@gmail.com>
2019-11-20 11:27:39 +0100

    All is ok Leidi !

--- target/classes/static/script.js
+++ target/classes/static/script.js
@@ -2,8 +2,8 @@
 
 // If set to true, the user must press
 // UP UP DOWN ODWN LEFT RIGHT LEFT RIGHT A B
-// to trigger the confetti with a random color theme.
-// Otherwise the confetti constantly falls.
+// to trigger the confetti with a random color theme
+// Otherwise the confetti constantly falls
 
 var onlyOnKonami = false;
 
